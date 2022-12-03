242be78f58a66b0df8693664fba28bb1714196a0
Lisa <bakosh21345@gmail.com>
2022-12-03 16:57:32 +0600

    a

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,11 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from taxiback.request import FireBaseRequest
-<<<<<<< HEAD:apps/users/signals.py
 from apps.users.models import Payment
-=======
-from users.models import Payment
->>>>>>> origin/release:users/signals.py
 
 
 @receiver(post_save, sender=Payment)
