d321ccb5fe9134a62eed7e17c7d2e780412b7dab
Lisa <bakosh21345@gmail.com>
2022-12-03 17:08:43 +0600

    a

--- taxiback/settings.py
+++ taxiback/settings.py
@@ -76,8 +76,12 @@ DATABASES = {
         "PORT": os.environ.get("SQL_PORT", "5432"),
     },
     'django_telemetry': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.telemetry.sqlite3',
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_TEL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_TEL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_TEL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_TEL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
