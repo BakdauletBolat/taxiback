b90b0a41e3640f7c3e585d21be647b82793eef6d
Lisa <bakosh21345@gmail.com>
2022-12-03 15:27:08 +0600

    message type

--- apps/message/admin.py
+++ apps/message/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from apps.message.models import Message
+from apps.message.models import Message, MessageType
 
 # Register your models here.
 admin.site.register(Message)
+admin.site.register(MessageType)
--- apps/message/models.py
+++ apps/message/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-
 from apps.users.models import User
+
+
+class MessageType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Message(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
+    type = models.ForeignKey(MessageType, on_delete=models.CASCADE)
     is_read = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
