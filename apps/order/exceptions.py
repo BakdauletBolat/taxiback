8c75b3b76644a60447fa06f69e65195a16c55ca1
Bolat <bakosh1345@gmail.com>
2023-11-06 23:38:37 +0600

    [main] added status_codes

--- apps/order/exceptions.py
+++ apps/order/exceptions.py
@@ -4,11 +4,12 @@ from rest_framework.exceptions import APIException, NotFound
 class TimeExpiredException(APIException):
 
     default_code = 'expired'
+    status_code = 430
 
 
 class NotEnoughBalanceException(APIException):
 
     default_code = 'notenough'
     default_detail = 'Не хватает баланса'
-    status_code = 422
+    status_code = 431
 
