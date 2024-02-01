c8db60f245a73b5138f4fb92308b4ff9e34616e1
Бакдаулет Болат <bakdauletbolat@Air-Bakdaulet.lan>
2024-02-01 18:29:17 +0600

    added index page

--- taxiback/urls.py
+++ taxiback/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
-from taxiback.views import render_privacy_view
+from taxiback.views import render_privacy_view, render_index
 
 urlpatterns = [
+    path('', render_index),
     path('admin/', admin.site.urls),
     path('privacy/', render_privacy_view),
     path('feedback/', include('form.urls')),
--- taxiback/views.py
+++ taxiback/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def render_privacy_view(request):
     return render(request, 'main/privacy.html')
+
+
+
+def render_index(request):
+    return render(request, 'main/index.html')
